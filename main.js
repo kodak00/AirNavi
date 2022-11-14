5e9fb8994a9e9cdb925f95c5f98c6725df7ddbc3
kodak00 <oskim68@gmail.com>
2022-11-14 21:41:44 +0900

    turn on cesium

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 const VportBtn = document.querySelector('.btn1');
 
 VportBtn.addEventListener('click', () => {
-  console.log('hi')
+  alert('click');
+});
+
+// Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
+const viewer = new Cesium.Viewer('cesiumContainer', {
+  terrainProvider: Cesium.createWorldTerrain()
+});    
+// Add Cesium OSM Buildings, a global 3D buildings layer.
+const buildingTileset = viewer.scene.primitives.add(Cesium.createOsmBuildings());   
+// Fly the camera to San Francisco at the given longitude, latitude, and height.
+viewer.camera.flyTo({
+  destination : Cesium.Cartesian3.fromDegrees(127.03, 37.4522, 400),
+  orientation : {
+    heading : Cesium.Math.toRadians(0.0),
+    pitch : Cesium.Math.toRadians(-15.0),
+  }
 });
