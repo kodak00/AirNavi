796ecfbb1ae2d169590b417d4f85c827a2cf949f
kodak00 <oskim68@gmail.com>
2022-12-04 17:01:41 +0900

    add vworld wmts in main screen

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 "use strict";
 
+//SameSite Error Handle
+document.cookie = "safeCookie1=foo; SameSite=Lax";
+document.cookie = "crossCookie=bar; SameSite=None; Secure";
+
 const psuBtn = document.querySelector('.btn_psu');
 
 psuBtn.onclick = function() {
@@ -10,7 +14,7 @@ psuBtn.onclick = function() {
 // psuBtn.addEventListener('click', () => {
 //   alert('click');
 // });
-
+/*
 //git test
 // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
 const viewer = new Cesium.Viewer('cesiumContainer', {
@@ -27,6 +31,54 @@ viewer.camera.flyTo({
     pitch : Cesium.Math.toRadians(-15.0),
   }
 });
+*/
+/////////////////////////////////////////////////////////////////////////
+const vworld_key = '0FBD6C52-3316-3CDE-A277-4EE6D00A2795';
+
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkZTY2YjdiNC0zMDZlLTQ1MzEtYWNlMS1jY2MyYjA1M2VhMTIiLCJpZCI6MTExMjE3LCJpYXQiOjE2NzAwNjk2MTV9.IQ6BZ9tDnlW--Nn4oESBVyAnBfDMP8xZYheTqI0Uy9g'
+
+var layers = [{layer : 'Base', tileType : 'png'},
+							{layer : 'gray', tileType : 'png'},
+							{layer : 'midnight', tileType : 'png'},
+							{layer : 'Hybrid', tileType : 'png'},
+							{layer : 'Satellite', tileType : 'jpeg'}]
+
+var selLayer = layers[4];
+
+var wms = new Cesium.WebMapTileServiceImageryProvider({
+				url : `http://api.vworld.kr/req/wmts/1.0.0/${vworld_key}/${selLayer.layer}/{TileMatrix}/{TileRow}/{TileCol}.${selLayer.tileType}`,
+				layer : 'Base',
+				style : 'default',
+				maximumLevel: 19,
+				credit : new Cesium.Credit('VWorld Korea')
+});
+
+var viewer = new Cesium.Viewer("cesiumContainer",{
+	timeline : false,
+	animation : false,
+	baseLayerPicker : false,
+	fullscreenButton: false,
+	geocoder: false,
+	homeButton: false,
+	infoBox: false,
+	sceneModePicker: false,
+	selectionIndicator: false,
+	navigationHelpButton: false,
+	navigationInstructionsInitiallyVisible: false,	
+	imageryProvider: wms,
+	terrainProvider : Cesium.createWorldTerrain({
+		requestVertexNormals : true
+	})
+});
+
+viewer.camera.flyTo({
+  destination : Cesium.Cartesian3.fromDegrees(127.03, 37.4522, 400),
+  orientation : {
+    heading : Cesium.Math.toRadians(0.0),
+    pitch : Cesium.Math.toRadians(-15.0),
+  }
+});
+
 ////////////////////////////////////////////////////////////////////////
 //var container = document.getElementById('popup'); //팝업이 담길 컨테이너 요소
 var map; //맵 변수 선언 : 지도 객체
@@ -41,11 +93,9 @@ function init(){
 			visible : true, //보여짐 여부
 			type : 'midnight', //지도 종류(일반) ---(야간(midnight), 위성(satellite) 등)
 			source : new ol.source.XYZ({ //vworld api 사용
-				url : 'http://api.vworld.kr/req/wmts/1.0.0/CEB52025-E065-364C-9DBA-44880E3B02B8/midnight/{z}/{y}/{x}.png'
+				url : `http://api.vworld.kr/req/wmts/1.0.0/${vworld_key}/midnight/{z}/{y}/{x}.png`
 			})
 	});
-
-	//mapOverlay = new ol.Overlay(({ element: container })); //Overlay 생성, 요소는 컨테이너
 
 	mapView =  new ol.View({ //뷰 생성
 			projection : 'EPSG:3857', //좌표계 설정 (EPSG:3857은 구글에서 사용하는 좌표계) 
